4339683839b81e97fe4ea772f28dd927dba10408
waraaaho <74201419+waraaaho@users.noreply.github.com>
2022-08-08 05:43:57 +0800

    Add files via upload

---
+++ Simple_static_code_analysis/sample2.py
@@ -0,0 +1,9 @@
+'''This module only has add function'''
+def add(number1, number2):
+    '''add two parameters'''
+    return number1 + number2
+
+Num1 = 4
+Num2 = 5
+Total = add(Num1, Num2)
+print("The sum of {} and {} is {}".format(Num1, Num2, Total))
